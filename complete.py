420c9cd02dc7790cd2e5d4e6e56b256b1d835a46
Hasnat Abdul <hasnatabdul5@gmail.com>
2022-02-12 22:16:28 +0000

    add nesting and fill_df functions

--- complete.py
+++ complete.py
@@ -4,9 +4,52 @@ from typing import List
 
 
 class Complete:
-    """ complete function to replication the complete() function in R dplyr """
+    """complete function to replication the complete() function in R dplyr"""
+
     def __init__(self, df: pd.DataFrame, nest, columns):
         self.df = df
         self.nest = nest
         self.columns = columns
         self.column_names = [col.name for col in columns]
+
+    def nesting(self):
+        """
+        """
+
+        return self.df[self.nest].drop_duplicates(keep="first").values.tolist()
+
+    def get_product(self):
+        """
+
+        """
+        product_list = []
+        product_list.append(self.nesting())
+        for series in self.columns:
+            product_list.append(list(set(series)))
+
+        col_list = ["nest"] + (self.column_names)
+
+        df = pd.DataFrame(list(product(*product_list)), columns=col_list)
+        return df
+
+    def fill_df(self):
+        """
+        """
+    
+        all_headers = self.nest + self.column_names
+        complete_df = self.get_product()
+
+        complete_df2 = pd.DataFrame(
+            complete_df["nest"].values.tolist(),
+            columns=self.nest)
+
+        complete_df2[self.column_names] = complete_df[self.column_names] 
+        del complete_df   
+        df_full = pd.merge(
+                complete_df2,
+                self.df,
+                how="left",
+                on=all_headers)
+    
+        return df_full
+
