e2f17851fa7d86fbf574a805c60c9a5267498aea
Hasnat Abdul <hasnatabdul5@gmail.com>
2022-02-12 22:07:31 +0000

    add initializer

--- complete.py
+++ complete.py
@@ -4,6 +4,9 @@ from typing import List
 
 
 class Complete:
-    """ complete function to replication the complete() function in R dplyr
-    """
-    pass
+    """ complete function to replication the complete() function in R dplyr """
+    def __init__(self, df: pd.DataFrame, nest, columns):
+        self.df = df
+        self.nest = nest
+        self.columns = columns
+        self.column_names = [col.name for col in columns]
