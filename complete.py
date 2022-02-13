b5b7f71ab31c30360885a3549491d99ae2950f45
Hasnat Abdul <hasnatabdul5@gmail.com>
2022-02-13 22:37:07 +0000

    add support for fillling null values

--- complete.py
+++ complete.py
@@ -6,7 +6,7 @@ from typing import List
 class Complete:
     """complete function to replication the complete() function in R dplyr"""
 
-    def __init__(self, df: pd.DataFrame, columns, nest=None, fill=None):
+    def __init__(self, df: pd.DataFrame, columns:List, nest=None, fill=None):
         self.df = df
         self.nest = nest
         if all(isinstance(x, str) for x in columns):
@@ -16,38 +16,42 @@ class Complete:
             self.columns = columns
             self.column_names = [col.name for col in columns]  
 
+        self.fill = fill    
+
     def nesting(self) -> List[str]:
         """
         Generate all possible existing combinations in the columns defind in `nest`
         """
         return self.df[self.nest].drop_duplicates(keep="first").values.tolist()
 
+    def product_without_nest(self) -> pd.DataFrame:
+        def get_product():
+            product_list = []
 
-    def get_product(self):
-        product_list = []
+            for series in self.columns:
+                product_list.append(list(set(series)))
 
-        for series in self.columns:
-            product_list.append(list(set(series)))
+            col_list = (self.column_names)
 
-        col_list = (self.column_names)
+            df = pd.DataFrame(list(product(*product_list)), columns=col_list)
+            return df
 
-        df = pd.DataFrame(list(product(*product_list)), columns=col_list)
-        return df
-
-    def fill_df(self):
-        """
-        """
+        def merge_product_to_df():
+            """
+            """
+        
+            all_headers = self.column_names
+            complete_df = self.get_product_2()
     
-        all_headers = self.column_names
-        complete_df = self.get_product_2()
- 
-        df_full = pd.merge(
-                complete_df,
-                self.df,
-                how="left",
-                on=all_headers)
-    
-        return df_full    
+            df_full = pd.merge(
+                    complete_df,
+                    self.df,
+                    how="left",
+                    on=all_headers)
+        
+            return df_full
+
+        return merge_product_to_df    
 
     def product_with_nest(self):
         def get_product():
@@ -64,7 +68,7 @@ class Complete:
             df = pd.DataFrame(list(product(*product_list)), columns=col_list)
             return df
 
-        def fill_df():
+        def merge_product_to_df():
             """
             """
         
@@ -85,15 +89,29 @@ class Complete:
         
             return df_full
         
-        return fill_df()
+        return merge_product_to_df()
+
+    
+    def fill_df(self, df):
+        for key in self.fill:
+            df[key] = df[key].fillna(self.fill[key])
+
+        return df    
+ 
 
 
     def run(self):
         """
         """
-        if self.nest !=None:
-            return self.product_with_nest()
-        elif self.nest == None:
-            print('w r ere')
-
+        if self.fill != None:
+            if self.nest != None:
+                return self.fill_df(self.product_with_nest())
+            elif self.nest == None:
+                return self.fill_df(self.product_without_nest())
+                
+        elif self.fill == None:
+            if self.nest != None:
+                return self.product_with_nest()
+            elif self.nest == None:
+                return self.product_without_nest()
 
